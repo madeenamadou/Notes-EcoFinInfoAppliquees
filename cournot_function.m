f02fb95d0bf68cd7237eb259d3d159f15998caa0
madeenamadou <youssef.amadou@yahoo.fr>
2017-02-23 14:02:43 -0500

    23feb1

--- cournot_function.m
+++ cournot_function.m
@@ -1,3 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   cournot.m:  A Matlab routine function that computes the value 
+%   fval and Jacobian fjac of the function at an arbitrary point x
+%   ----
+%   Youssef de Madeen Amadou, Winter 2014
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 function [fval,fjac] = cournot(q)
     c = [0.6; 0.8]; eta = 1.6; e = -1/eta;
     fval = sum(q)^e + e*sum(q)^(e-1)*q - diag(c)*q;
